11bab1ebeead63e6f7c3235dfe656833023982d8
Balaji Naik <balaji@habi.one>
2025-08-22 12:35:31 +0000

    logo is not appeared

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Logo = ({ className }: { className?: string }) => {
   return (
     <div className={className}>
-        <Image src="/image/logo.png" alt="Astramaan Logo" width={132} height={50} priority />
+        <Image src="/image/logo.png" alt="AstraCore Logo" width={132} height={50} priority />
     </div>
   );
 };
