c4ea75c3ac1e3ce99a78e773a7bb6d7a160670b7
Balaji Naik <balaji@habi.one>
2025-08-22 12:08:49 +0000

    Temprory comment down the notch

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -1,5 +1,4 @@
 
-
 'use client';
 
 import React from 'react';
@@ -116,7 +115,7 @@ const PlatformNav = () => {
         <nav className="fixed bottom-4 left-0 right-0 z-20 px-4 sm:px-6 lg:px-8">
              <div className="container mx-auto">
                 <div className="bg-neutral-900/20 rounded-[50px] border border-gray-300 backdrop-blur-[5px] p-2 relative">
-                    <div className="absolute -top-1.5 left-1/2 -translate-x-1/2 w-16 h-3 bg-neutral-900/20 rounded-b-xl border-b border-l border-r border-gray-300" />
+                    {/* <div className="absolute -top-1.5 left-1/2 -translate-x-1/2 w-16 h-3 bg-neutral-900/20 rounded-b-xl border-b border-l border-r border-gray-300" /> */}
                     <div className="flex items-center justify-between">
                         {navItems.map((item) => (
                             <Link href={item.href} key={item.label}>
