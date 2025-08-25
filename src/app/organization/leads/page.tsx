143fb7260f8c9ceccea69933a8bf3da490ba8cd6
Balaji Naik <balaji@habi.one>
2025-08-25 11:09:05 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <FloatingActionBar

--- src/app/organization/leads/page.tsx
+++ src/app/organization/leads/page.tsx
@@ -96,7 +96,7 @@ const FloatingActionBar = ({ selectedCount, onSelectAll, allSelected, clearSelec
     if (selectedCount === 0) return null;
 
     return (
-        <div className="fixed bottom-8 left-1/2 -translate-x-1/2 w-[828px] h-20 bg-white rounded-[50px] shadow-[-5px_-5px_25px_0px_rgba(17,17,17,0.25)] flex items-center justify-between px-6 z-50">
+        <div className="fixed bottom-28 left-1/2 -translate-x-1/2 w-[828px] h-20 bg-white rounded-[50px] shadow-[-5px_-5px_25px_0px_rgba(17,17,17,0.25)] flex items-center justify-between px-6 z-50">
             <div className="flex items-center gap-4">
                 <Checkbox id="select-all-floating" checked={allSelected} onCheckedChange={(checked) => onSelectAll(!!checked)} />
                 <label htmlFor="select-all-floating" className="text-lg font-medium">{allSelected ? 'Deselect all' : 'Select all'}</label>
@@ -133,7 +133,7 @@ export default function LeadsPage() {
         );
     };
 
-    const allLeadsSelected = useMemo(() => selectedLeads.length === leads.length, [selectedLeads.length]);
+    const allLeadsSelected = useMemo(() => selectedLeads.length === leads.length && leads.length > 0, [selectedLeads.length, leads.length]);
 
     const handleSelectAll = (checked: boolean) => {
         if (checked) {
