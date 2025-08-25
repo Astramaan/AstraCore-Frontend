af35953e458ec2e277146788fe859b3926b3123a
Balaji Naik <balaji@habi.one>
2025-08-25 12:04:26 +0000

    still it is behaving like that only

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="overflow-y-scroll">
       <body className={cn("antialiased font-sans", gilroy.variable)}>
         {children}
         <Toaster />
