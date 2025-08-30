4e00cf1112cfefb3115585c9728526aa60d51dc2
Balaji Naik <balaji@habi.one>
2025-08-30 08:31:26 +0000

    Make the following changes:
    On the page src/app/organization/leads/page

--- src/components/add-lead-sheet.tsx
+++ src/components/add-lead-sheet.tsx
@@ -111,7 +111,7 @@ export function AddLeadSheet() {
                                 Add New Lead
                             </DialogOrSheetTitle>
                             <DialogOrSheetClose asChild>
-                                <Button variant="ghost" size="icon" className="p-3.5 bg-background rounded-full">
+                                <Button variant="ghost" size="icon" className="w-[54px] h-[54px] bg-background rounded-full">
                                     <X className="h-6 w-6" />
                                 </Button>
                             </DialogOrSheetClose>
