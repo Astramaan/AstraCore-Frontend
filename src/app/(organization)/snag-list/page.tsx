038fad02adf223d8d8471a3dbcbe414eecd4ebbc
Balaji Naik <balaji@habi.one>
2025-08-31 12:53:31 +0000

    Look the vertical lines is not align. see the leads and projects vertica

--- src/app/(organization)/snag-list/page.tsx
+++ src/app/(organization)/snag-list/page.tsx
@@ -97,14 +97,14 @@ const allSnagsData: Snag[] = [
 
 const SnagCard = ({ snag, onSelectionChange, isSelected, onSingleDelete }: { snag: Snag, onSelectionChange: (id: string, checked: boolean) => void, isSelected: boolean, onSingleDelete: (id: string) => void }) => (
     <div className="flex flex-col">
-        <div className="grid grid-cols-[auto_1fr_auto_1fr_auto_auto] items-center py-4 gap-x-4">
-            <Checkbox 
-                id={`select-${snag.id}`} 
-                className="w-6 h-6 rounded-full" 
-                checked={isSelected}
-                onCheckedChange={(checked) => onSelectionChange(snag.id, !!checked)}
-            />
-            <div className="flex items-center gap-4">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center py-4 gap-4">
+            <div className="flex items-center gap-4 flex-1">
+                <Checkbox 
+                    id={`select-${snag.id}`} 
+                    className="w-6 h-6 rounded-full" 
+                    checked={isSelected}
+                    onCheckedChange={(checked) => onSelectionChange(snag.id, !!checked)}
+                />
                 <Image src={snag.image} alt={snag.title} width={60} height={60} className="rounded-lg" data-ai-hint="defect photo"/>
                 <div className="flex flex-col gap-1 w-full md:w-60">
                     <p className="font-medium text-lg text-black">{snag.title}</p>
@@ -115,7 +115,7 @@ const SnagCard = ({ snag, onSelectionChange, isSelected, onSingleDelete }: { sna
 
             <div className="w-px h-14 bg-stone-200 hidden md:block" />
 
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 flex-1">
                 <p className="text-lg"><span className="text-grey-1">Created By: </span><span className="text-black font-medium">{snag.createdBy}</span></p>
                 <p className="text-sm text-grey-1">{snag.createdAt}</p>
             </div>
