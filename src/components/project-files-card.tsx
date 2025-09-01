c562dcc6f738b2a19130d7fabbe9d94e2a1db9c7
Balaji Naik <balaji@habi.one>
2025-09-01 11:08:02 +0000

    In Desing & Documents Card -Give the Padding 40px and remove the borderl

--- src/components/project-files-card.tsx
+++ src/components/project-files-card.tsx
@@ -58,11 +58,11 @@ const FileSection = ({ title, files }: { title: string, files: File[] }) => (
 
 export const ProjectFilesCard = ({ files }: ProjectFilesCardProps) => {
     return (
-        <Card className="rounded-[50px] border border-stone-300">
-            <CardHeader className="flex flex-row justify-between items-center">
+        <Card className="rounded-[50px] p-10 border-0">
+            <CardHeader className="p-0 flex flex-row justify-between items-center">
                 <CardTitle className="text-lg">Design & Documents</CardTitle>
             </CardHeader>
-            <CardContent className="space-y-6">
+            <CardContent className="p-0 mt-6 space-y-6">
                 <FileSection title="Initial" files={files.initial} />
                 <Separator />
                 <FileSection title="Costing" files={files.costing} />
