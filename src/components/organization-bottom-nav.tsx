85a47c3999470d2ef836825f74c60df2a8be0a2c
Balaji Naik <balaji@habi.one>
2025-09-08 12:14:50 +0000

    buttons should be rounded 54px the gap should be fit to the mobile scree

--- src/components/organization-bottom-nav.tsx
+++ src/components/organization-bottom-nav.tsx
@@ -23,13 +23,13 @@ export const OrganizationBottomNav = () => {
     return (
         <div className="fixed bottom-4 md:bottom-8 inset-x-0 z-10 px-2 md:px-4 w-full">
              <div className="relative mx-auto h-auto bg-neutral-900/20 rounded-[50px] border border-grey-1 backdrop-blur-[5px] py-4 px-4 max-w-5xl">
-                <div className="flex items-center justify-around gap-4">
+                <div className="flex items-center justify-between gap-2">
                     {navItems.map((item) => {
                         const isActive = pathname.startsWith(item.href);
                         return (
                              <Link href={item.href} key={item.label} className="flex-1 md:flex-none">
                                 <div className={cn(
-                                    "flex flex-col md:flex-row items-center justify-center text-center gap-1 transition-colors duration-200 h-full w-full",
+                                    "flex flex-col md:flex-row items-center justify-center text-center gap-1 transition-colors duration-200",
                                     "md:gap-2.5 md:py-3 md:px-5 md:rounded-[50px] min-w-max",
                                     "h-auto w-auto p-2 rounded-full",
                                     isActive ? "bg-primary text-white" : "bg-white text-black hover:bg-white hover:text-primary"
