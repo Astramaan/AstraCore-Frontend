403b2b4b0d6999b39b6bff114f8beeeec9011a34
Balaji Naik <balaji@habi.one>
2025-09-10 12:34:54 +0000

    -webkit-text-size-adjust' is not supported by Chrome, Chrome Android, Ed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,21 +84,21 @@ export default {
       keyframes: {
         'accordion-down': {
           from: {
-            height: '0',
+            maxHeight: '0',
             opacity: '0'
           },
           to: {
-            height: 'var(--radix-accordion-content-height)',
+            maxHeight: 'var(--radix-accordion-content-height)',
             opacity: '1'
           },
         },
         'accordion-up': {
           from: {
-            height: 'var(--radix-accordion-content-height)',
+            maxHeight: 'var(--radix-accordion-content-height)',
             opacity: '1'
           },
           to: {
-            height: '0',
+            maxHeight: '0',
             opacity: '0'
           },
         },
