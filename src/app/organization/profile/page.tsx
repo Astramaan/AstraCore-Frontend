4c2e8631c35a93bb0486576a5d92d545380b1e5b
Balaji Naik <balaji@habi.one>
2025-09-12 05:02:50 +0000

    Hover effect - Text and icon Red and the backround red/10 (_for element

--- src/app/organization/profile/page.tsx
+++ src/app/organization/profile/page.tsx
@@ -35,7 +35,7 @@ export default function ProfilePage() {
                 <div className="flex flex-col gap-6">
                     <ActiveSessionsCard />
                     <div className="flex justify-end">
-                        <Button variant="outline" asChild className="rounded-full h-[54px] px-10 text-lg bg-white hover:bg-primary/10 hover:text-primary w-full">
+                        <Button variant="outline" asChild className="rounded-full h-[54px] px-10 text-lg bg-white hover:bg-destructive/10 hover:text-destructive w-full">
                            <Link href="/">
                              <LogOut className="mr-2 h-5 w-5" />
                              Logout
