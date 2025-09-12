60bd690d592d9dc3f57cd23e100cdf005341493a
Balaji Naik <balaji@habi.one>
2025-09-12 10:57:38 +0000

    right side vertical line moves to right side.

--- src/app/organization/projects/page.tsx
+++ src/app/organization/projects/page.tsx
@@ -45,7 +45,7 @@ const ProjectListItem = ({ project, onEdit, onDelete, isFirst = false, isLast = 
             
             <Separator orientation="vertical" className="h-14 hidden md:block" />
             
-            <div className="flex-1 md:pl-8 pl-16">
+            <div className="flex-[1.5] md:pl-8 pl-16">
                  <div className="flex flex-col gap-2">
                     <p className="text-lg whitespace-nowrap"><span className="text-grey-2">Contact: </span><span className="text-black">{project.contact}</span></p>
                     <p className="text-lg"><span className="zinc-900">{project.id}</span></p>
