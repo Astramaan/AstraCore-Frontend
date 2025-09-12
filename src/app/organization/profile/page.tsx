7630f66de7548fd9531a1957a809dac02f1a534c
Balaji Naik <balaji@habi.one>
2025-09-12 04:54:34 +0000

    Expand the height like Active sessions and adjust the gap between the bu

--- src/app/organization/profile/page.tsx
+++ src/app/organization/profile/page.tsx
@@ -29,7 +29,7 @@ export default function ProfilePage() {
             </div>
             <PersonalDetails memberId="1" />
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-                <div className="lg:col-span-2">
+                <div className="lg:col-span-2 flex">
                     <FeatureAccessCard />
                 </div>
                 <div className="flex flex-col gap-6">
