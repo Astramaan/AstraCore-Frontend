40f3adc7176b24a2784fa0c7b1a01c22759da38f
Balaji Naik <balaji@habi.one>
2025-09-12 06:38:55 +0000

    Fix this and should be like lead list equal space between the containers

--- src/app/organization/meetings/page.tsx
+++ src/app/organization/meetings/page.tsx
@@ -83,13 +83,13 @@ const MeetingListItem = ({ meeting, onEdit, onDelete, onViewDetails, isLast = fa
                     <p className="text-lg"><span className="text-grey-1">City: </span><span className="text-black font-medium">{meeting.city}</span></p>
                 </div>
             </div>
-            <div className="w-px h-14 bg-stone-300/0 md:bg-stone-300" />
-            <div className="flex flex-col gap-2 flex-1 pl-16 md:pl-0">
+            <div className="w-px h-14 bg-stone-300 hidden md:block" />
+            <div className="flex flex-col gap-2 flex-1 md:pl-0">
                 <p className="text-lg whitespace-nowrap"><span className="text-grey-2">Contact: </span><span className="text-black">{meeting.email} | {meeting.phone}</span></p>
                 <p className="text-lg"><span className="text-grey-2">{meeting.type === 'lead' ? 'Lead ID' : 'Client ID'}: </span><span className="text-zinc-900">{meeting.id}</span></p>
             </div>
-            <div className="w-px h-14 bg-stone-300/0 md:bg-stone-300" />
-            <div className="h-auto flex-col justify-between items-start inline-flex flex-1 pl-16 md:pl-0">
+            <div className="w-px h-14 bg-stone-300 hidden md:block" />
+            <div className="flex-1 flex flex-col justify-between items-start md:pl-0">
                 <p className="text-lg whitespace-nowrap"><span className="text-grey-2">Date & Time : </span><span className="text-zinc-900">{meeting.date}, {meeting.time}</span></p>
                 <div className="flex items-center gap-2 text-lg">
                     <span className="text-grey-2">Link: </span> 
@@ -108,7 +108,7 @@ const MeetingListItem = ({ meeting, onEdit, onDelete, onViewDetails, isLast = fa
                     </DropdownMenuTrigger>
                     <DropdownMenuContent onClick={(e) => e.stopPropagation()}>
                         <DropdownMenuItem onSelect={(e) => { e.stopPropagation(); onEdit(meeting); }}>Edit</DropdownMenuItem>
-                        <DropdownMenuItem onSelect={(e) => { e.stopPropagation(); onDelete(meeting); }} className="text-red-500">Delete</DropdownMenuItem>
+                        <DropdownMenuItem onSelect={(e) => { e.preventDefault(); e.stopPropagation(); onDelete(meeting); }} className="text-red-500">Delete</DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
