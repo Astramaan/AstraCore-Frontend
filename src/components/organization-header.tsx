bbadd17eeabc101aa36d3e41cc835bd2153a7e28
Balaji Naik <balaji@habi.one>
2025-09-13 05:28:49 +0000

    I didn't mention to hide the title, change the size of headline to 24px

--- src/components/organization-header.tsx
+++ src/components/organization-header.tsx
@@ -47,7 +47,7 @@ export const OrganizationHeader = () => {
              {pageTitle && (
                 <>
                     <div className="w-px h-8 bg-stone-300 hidden lg:block" />
-                    <h2 className="hidden lg:block text-2xl md:text-[40px] font-semibold md:font-bold text-zinc-900">
+                    <h2 className="hidden md:block text-2xl font-semibold text-zinc-900">
                         {pageTitle}
                     </h2>
                 </>
