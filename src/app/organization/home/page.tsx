d7fc8a0765a4b456985fd5136036503cad91fb9b
Balaji Naik <balaji@habi.one>
2025-09-13 08:04:35 +0000

    Do for meetings also only in tab only

--- src/app/organization/home/page.tsx
+++ src/app/organization/home/page.tsx
@@ -278,7 +278,7 @@ export default function OrganizationHomePage({ searchParams }: { searchParams: {
                         see all meetings
                     </Link>
                 </div>
-                <div className="flex flex-col gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4">
                     {meetings.map(meeting => <MeetingCard key={meeting.id} meeting={meeting} onClick={handleMeetingClick} />)}
                 </div>
             </div>
