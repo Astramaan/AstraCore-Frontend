1ece5e46d8b13e07e14632a1b5e6e94d8202ca9a
Balaji Naik <balaji@habi.one>
2025-09-14 12:17:39 +0000

    Give the padding outside of the hover. (_for element src/app/organizatio

--- src/app/organization/leads/page.tsx
+++ src/app/organization/leads/page.tsx
@@ -109,17 +109,12 @@ const leadsData: Lead[] = [
 const LeadCard = ({ lead, onSelectionChange, isSelected, onSingleDelete, onContact, onViewDetails, onLevelChange, onEdit, isFirst, isLast }: { lead: Lead, onSelectionChange: (id: string, checked: boolean) => void, isSelected: boolean, onSingleDelete: (id: string) => void, onContact: (lead: Lead) => void, onViewDetails: (lead: Lead) => void, onLevelChange: (leadId: string, level: string) => void, onEdit: (lead: Lead) => void, isFirst?: boolean, isLast?: boolean }) => (
     <div className="flex flex-col group">
         {/* Desktop View */}
-        <div className={cn(
-                "hidden lg:block p-6",
-                isFirst && "rounded-t-[30px]",
-                isLast && "rounded-b-[30px]",
-                "hover:bg-hover-bg"
-            )}>
-            <div
-            className={cn(
-                "grid lg:grid-cols-[1.2fr_1.5fr_1fr] items-stretch cursor-pointer",
-            )}
-            >
+        <div className="hidden lg:block p-6">
+            <div className={cn(
+                    "grid lg:grid-cols-[1.2fr_1.5fr_1fr] items-stretch cursor-pointer hover:bg-hover-bg",
+                    isFirst && "rounded-t-[30px]",
+                    isLast && "rounded-b-[30px]",
+                )}>
                     {/* Col 1: Name + Location */}
                     <div
                         onClick={() => onViewDetails(lead)}
@@ -454,7 +449,7 @@ export default function LeadsPage({ searchParams }: { searchParams: { [key: stri
             </div>
 
              <AlertDialog open={isDeleteConfirmationOpen} onOpenChange={setIsDeleteConfirmationOpen}>
-                <div className="flex flex-col bg-white rounded-[30px] overflow-hidden">
+                <div className="flex flex-col bg-white rounded-[30px] overflow-hidden lg:p-6">
                     {filteredLeads.map((lead, index) => (
                         <LeadCard 
                             key={lead.leadId} 
