33dde2fb4db42c006d9161747803dd135b5eb0f3
Balaji Naik <balaji@habi.one>
2025-09-15 06:52:21 +0000

    ?

--- src/components/view-vendors-sheet.tsx
+++ src/components/view-vendors-sheet.tsx
@@ -38,7 +38,7 @@ const VendorCard = ({ vendor, materialName }: { vendor: Vendor; materialName: st
         <>
             <div className="flex flex-col">
                 {/* Mobile View */}
-                <div className="md:hidden flex justify-between items-start p-10 gap-4">
+                <div className="md:hidden flex justify-between items-start p-6 gap-4">
                     <div className="flex items-center gap-4 flex-1">
                         <Link href={`/organization/vendors/${vendor.id}`} className="flex items-center gap-4 cursor-pointer">
                             <Avatar className="w-12 h-12">
@@ -145,7 +145,7 @@ const ViewVendorsContent = ({ material, onClose }: { material: Material; onClose
                     </SheetTitle>
                 </SheetHeader>
                 <ScrollArea className="flex-1">
-                    <div className="p-6">
+                    <div className="p-0 md:p-6">
                         {material.vendors.length > 0 ? (
                             material.vendors.map((vendor) => <VendorCard key={vendor.id} vendor={vendor} materialName={material.name}/>)
                         ) : (
@@ -174,8 +174,10 @@ export function ViewVendorsSheet({ isOpen, onClose, material }: ViewVendorsSheet
     return (
          <Sheet open={isOpen} onOpenChange={onClose}>
             <SheetContent 
-                side={"bottom"}
-                className="p-0 bg-transparent border-none shadow-none w-full h-[90vh] bottom-0 rounded-t-[50px]"
+                side={isMobile ? "bottom" : "bottom"}
+                className={cn(
+                    "p-0 bg-transparent border-none shadow-none w-full md:max-w-5xl md:mx-auto h-[90vh] bottom-0 rounded-t-[50px]",
+                )}
                 overlayClassName="bg-neutral-900/10 backdrop-blur-sm"
             >
                 <ViewVendorsContent material={material} onClose={onClose} />
