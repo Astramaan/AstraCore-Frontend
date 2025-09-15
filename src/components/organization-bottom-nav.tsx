fc302fb3a9a1b8fe7558f5ea9b31aa5c8de88361
Balaji Naik <balaji@habi.one>
2025-09-15 06:20:34 +0000

    in mobile screen make the icon above the text only in mobile screen. (_f

--- src/components/organization-bottom-nav.tsx
+++ src/components/organization-bottom-nav.tsx
@@ -32,9 +32,9 @@ export const OrganizationBottomNav = () => {
                         return (
                              <Link href={item.href} key={item.label} title={item.label} className="flex-shrink-0">
                                 <div className={cn(
-                                    "flex flex-row items-center justify-center text-center gap-1.5 transition-colors duration-200",
+                                    "flex flex-col md:flex-row items-center justify-center text-center gap-0 md:gap-1.5 transition-colors duration-200",
                                     "lg:gap-2.5 md:py-3 md:px-5 md:rounded-full min-w-max",
-                                    "h-12 w-auto px-4 rounded-full lg:h-[54px]",
+                                    "h-16 w-20 md:h-12 md:w-auto px-1 md:px-4 rounded-full lg:h-[54px]",
                                     isActive ? "bg-primary text-white" : "bg-white text-black hover:bg-white hover:text-primary"
                                 )}>
                                     <item.icon className="w-5 h-5 lg:w-6 lg:h-6 shrink-0" />
