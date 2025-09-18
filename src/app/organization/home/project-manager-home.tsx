af65511cc05aa16e77c4fc60db2e9265a539843e
Balaji Naik <balaji@habi.one>
2025-09-18 05:01:27 +0000

    ?

--- src/app/organization/home/project-manager-home.tsx
+++ src/app/organization/home/project-manager-home.tsx
@@ -319,7 +319,7 @@ export default function ProjectManagerHome() {
                         />
                     )}
                 </div>
-                <div className="mt-16">
+                <div className="mt-24">
                     <div className="hidden lg:flex items-center gap-4 overflow-x-auto pb-2 -mx-4 px-4 w-full lg:w-auto">
                         {['High Priority', 'In Progress', 'Pending', 'Completed'].map(filter => (
                             <Button
@@ -362,7 +362,7 @@ export default function ProjectManagerHome() {
                             </DropdownMenuContent>
                         </DropdownMenu>
                     </div>
-                    <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center mt-6 mb-6 gap-4">
+                    <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center mt-8 mb-6 gap-4">
                          <h2 className="text-xl font-medium text-left">My Task</h2>
                     </div>
                    
