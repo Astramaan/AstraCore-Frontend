926a323d0ca30ef1e8c0af514ba69051e325ccf4
Balaji Naik <balaji@habi.one>
2025-09-18 11:07:52 +0000

    make the border corner radius 50px  (_for element div_)

--- src/app/organization/[organizationId]/newuser/[newuserId]/home/page.tsx
+++ src/app/organization/[organizationId]/newuser/[newuserId]/home/page.tsx
@@ -60,7 +60,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
         <div className="bg-zinc-100 min-h-screen pb-32 md:pb-40">
             <main>
                 <div className="max-w-[1240px] mx-auto py-8 px-4">
-                    <Card className="w-full p-4 md:p-8 bg-white rounded-2xl flex flex-col justify-center items-center">
+                    <Card className="w-full p-4 md:p-8 bg-white rounded-[50px] flex flex-col justify-center items-center">
                         <h1 className="text-center text-neutral-900 text-2xl font-medium leading-none mb-6">Book Free Consultation</h1>
                         <Card className="w-full max-w-3xl rounded-[20px] border border-stone-300 p-6 flex flex-col justify-center items-center">
                             <h2 className="text-center text-black text-lg font-medium leading-tight mb-4">Connect with Us!</h2>
@@ -71,7 +71,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
                         </Card>
                     </Card>
 
-                    <Card className="w-full mt-8 p-4 md:p-8 bg-white rounded-2xl">
+                    <Card className="w-full mt-8 p-4 md:p-8 bg-white rounded-[50px]">
                         <CardContent className="p-0">
                              <h2 className="text-center text-black text-lg font-normal leading-tight mb-8">Constructing Dreams with Precision and Care</h2>
                              <div className="grid grid-cols-2 md:grid-cols-4 gap-y-8 gap-x-4 mb-8 justify-items-center">
@@ -101,7 +101,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
                         </CardContent>
                     </Card>
 
-                    <Card className="w-full mt-8 p-4 md:p-8 bg-white rounded-2xl">
+                    <Card className="w-full mt-8 p-4 md:p-8 bg-white rounded-[50px]">
                          <CardContent className="p-0">
                             <h2 className="text-center text-black text-lg font-medium mb-8">FAQ’s</h2>
                             <Accordion type="single" collapsible className="w-full max-w-3xl mx-auto">
