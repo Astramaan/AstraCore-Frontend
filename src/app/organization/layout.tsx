25103f901cf875278796d95fcadf5986791a8925
Balaji Naik <balaji@habi.one>
2025-09-18 10:53:04 +0000

    Remove this too. (_for element <OrganizationHeader>_)

--- src/app/organization/layout.tsx
+++ src/app/organization/layout.tsx
@@ -1,9 +1,6 @@
-
-
 'use client';
 
 import React, from 'react';
-import { OrganizationHeader } from '@/components/organization-header';
 import { OrganizationBottomNav } from '@/components/organization-bottom-nav';
 import { UserProvider } from '@/context/user-context';
 
@@ -12,7 +9,6 @@ function OrganizationLayoutContent({ children }: { children: React.ReactNode }) 
   return (
     <div className="min-h-screen bg-background">
         <main className="max-w-[1440px] mx-auto w-full flex-1 overflow-y-auto bg-background pb-32 md:pb-40 p-4 space-y-6">
-            <OrganizationHeader />
             {children}
         </main>
         <OrganizationBottomNav />
