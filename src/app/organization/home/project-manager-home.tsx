2cd1c62901a56daed8711d284d7eb39828e2d3ee
Balaji Naik <balaji@habi.one>
2025-09-18 04:58:53 +0000

    Give more gap top side (_for element div_)

--- src/app/organization/home/project-manager-home.tsx
+++ src/app/organization/home/project-manager-home.tsx
@@ -319,7 +319,7 @@ export default function ProjectManagerHome() {
                         />
                     )}
                 </div>
-                <div className="mt-12">
+                <div className="mt-16">
                     <div className="hidden lg:flex items-center gap-4 overflow-x-auto pb-2 -mx-4 px-4 w-full lg:w-auto">
                         {['High Priority', 'In Progress', 'Pending', 'Completed'].map(filter => (
                             <Button
@@ -427,5 +427,7 @@ export default function ProjectManagerHome() {
 
 
 
+
+    
 
     
