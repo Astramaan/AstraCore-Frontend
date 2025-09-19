ec5178aa8eb96c7f967237780594cad1eb198f05
Balaji Naik <balaji@habi.one>
2025-09-19 06:33:34 +0000

    Remove the padding and margin from this div in mobile screen only. (_for

--- src/app/organization/[organizationId]/client/[newuserId]/home/page.tsx
+++ src/app/organization/[organizationId]/client/[newuserId]/home/page.tsx
@@ -59,8 +59,8 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
     return (
         <div className="bg-zinc-100 min-h-screen pb-32">
             <main>
-                <div className="max-w-[1240px] mx-auto py-8 px-4">
-                    <Card className="w-full p-10 bg-white rounded-[50px] flex flex-col justify-center items-center">
+                <div className="max-w-[1240px] mx-auto py-8 px-0 md:px-4">
+                    <Card className="w-full p-4 md:p-10 bg-white rounded-none md:rounded-[50px] flex flex-col justify-center items-center">
                         <h1 className="text-center text-neutral-900 text-2xl font-medium leading-none mb-6">Book Free Consultation</h1>
                         <Card className="w-full max-w-3xl rounded-[50px] border border-stone-300 p-6 flex flex-col justify-center items-center">
                             <h2 className="text-center text-black text-lg font-medium leading-tight mb-4">Connect with Us!</h2>
@@ -71,7 +71,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
                         </Card>
                     </Card>
 
-                    <Card className="w-full mt-8 p-4 md:p-8 bg-white rounded-[50px]">
+                    <Card className="w-full mt-8 p-4 md:p-8 bg-white rounded-none md:rounded-[50px]">
                         <CardContent className="p-0">
                              <h2 className="text-center text-black text-lg font-normal leading-tight mb-8">Constructing Dreams with Precision and Care</h2>
                              <div className="relative mb-8">
@@ -107,7 +107,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
                         </CardContent>
                     </Card>
 
-                    <Card className="w-full mt-8 p-4 md:p-8 bg-white rounded-[50px]">
+                    <Card className="w-full mt-8 p-4 md:p-8 bg-white rounded-none md:rounded-[50px]">
                          <CardContent className="p-0">
                             <h2 className="text-center text-black text-lg font-medium mb-8">FAQ’s</h2>
                             <Accordion type="single" collapsible className="w-full max-w-3xl mx-auto space-y-2">
@@ -148,5 +148,3 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
         </div>
     )
 }
-
-    
