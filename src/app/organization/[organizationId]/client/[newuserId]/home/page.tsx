b19349250698163cde8e294092028ba8d8f2ebdf
Balaji Naik <balaji@habi.one>
2025-09-19 06:43:45 +0000

    For this too. (_for element div_)

--- src/app/organization/[organizationId]/client/[newuserId]/home/page.tsx
+++ src/app/organization/[organizationId]/client/[newuserId]/home/page.tsx
@@ -60,7 +60,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
         <div className="bg-zinc-100 min-h-screen">
             <main>
                 <div className="max-w-[1240px] mx-auto px-0 md:px-4">
-                    <Card className="text-card-foreground w-full p-4 md:p-10 bg-white rounded-b-[50px] md:rounded-[50px] flex flex-col justify-start items-center">
+                    <Card className="rounded-lg text-card-foreground w-full p-4 md:p-10 bg-white rounded-[50px] flex flex-col justify-start items-center">
                         <h1 className="text-center text-neutral-900 text-2xl font-medium leading-none mb-6">Book Free Consultation</h1>
                         <Card className="w-full max-w-3xl rounded-[50px] border border-stone-300 p-6 flex flex-col justify-center items-center">
                             <h2 className="text-center text-black text-lg font-medium leading-tight mb-4">Connect with Us!</h2>
@@ -71,7 +71,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
                         </Card>
                     </Card>
 
-                    <Card className="w-full mt-8 p-4 md:p-8 bg-white rounded-[50px] md:rounded-[50px]">
+                    <Card className="text-card-foreground w-full mt-8 p-4 md:p-8 bg-white rounded-[50px]">
                         <CardContent className="p-0">
                              <h2 className="text-center text-black text-lg font-normal leading-tight mb-8">Constructing Dreams with Precision and Care</h2>
                              <div className="relative mb-8">
