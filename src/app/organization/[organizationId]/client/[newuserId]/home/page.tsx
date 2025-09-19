ad25e361c122e7752d66720c6fd2adf9fc8d42ed
Balaji Naik <balaji@habi.one>
2025-09-19 06:38:13 +0000

    top align in mobile screen (_for element div_)

--- src/app/organization/[organizationId]/client/[newuserId]/home/page.tsx
+++ src/app/organization/[organizationId]/client/[newuserId]/home/page.tsx
@@ -60,7 +60,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
         <div className="bg-zinc-100 min-h-screen pb-32">
             <main>
                 <div className="max-w-[1240px] mx-auto py-8 px-0 md:px-4">
-                    <Card className="w-full p-4 md:p-10 bg-white rounded-none md:rounded-[50px] flex flex-col justify-center items-center">
+                    <Card className="text-card-foreground w-full p-4 md:p-10 bg-white rounded-none md:rounded-[50px] flex flex-col justify-start items-center">
                         <h1 className="text-center text-neutral-900 text-2xl font-medium leading-none mb-6">Book Free Consultation</h1>
                         <Card className="w-full max-w-3xl rounded-[50px] border border-stone-300 p-6 flex flex-col justify-center items-center">
                             <h2 className="text-center text-black text-lg font-medium leading-tight mb-4">Connect with Us!</h2>
