357a4ff13fc6e67fa0e234574dd1918baddc3380
Balaji Naik <balaji@habi.one>
2025-09-19 07:21:12 +0000

    Change to "In Person" (_for element <Button>_)

--- src/app/organization/[organizationId]/client/[newuserId]/home/page.tsx
+++ src/app/organization/[organizationId]/client/[newuserId]/home/page.tsx
@@ -90,7 +90,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
                         <Card className="w-full max-w-3xl rounded-[50px] border border-stone-300 p-6 flex flex-col justify-center items-center">
                             <h2 className="text-center text-black text-lg font-medium leading-tight mb-4">Connect with Us!</h2>
                             <div className="flex flex-col md:flex-row gap-4 w-full md:w-auto">
-                                <Button className="w-full md:w-64 h-[54px] rounded-full text-lg">Physically</Button>
+                                <Button className="w-full md:w-64 h-[54px] rounded-full text-lg">In Person</Button>
                                 <Button className="w-full md:w-64 h-[54px] rounded-full text-lg">Virtually</Button>
                             </div>
                         </Card>
