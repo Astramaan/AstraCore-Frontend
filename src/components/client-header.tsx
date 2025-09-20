5a34374d24b58b11016ef752c377824976b031fd
Balaji Naik <balaji@habi.one>
2025-09-20 07:20:04 +0000

    make it 24px (_for element h2_)

--- src/components/client-header.tsx
+++ src/components/client-header.tsx
@@ -37,7 +37,7 @@ export const ClientHeader = () => {
                 {pageTitle && (
                     <>
                         <div className="w-px h-8 bg-stone-300 hidden md:block" />
-                        <h2 className="hidden md:block text-xl md:text-2xl lg:text-[40px] lg:leading-[48px] font-semibold text-zinc-900">
+                        <h2 className="hidden md:block text-2xl font-semibold text-zinc-900">
                            {pageTitle}
                         </h2>
                     </>
