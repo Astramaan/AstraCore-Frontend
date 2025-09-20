c3cbc0f434458fcae053462d6ac83c1e573635fc
Balaji Naik <balaji@habi.one>
2025-09-20 09:41:27 +0000

    how to access the existingclient give the link structure

--- src/app/organization/[organizationId]/(internal)/home/page.tsx
+++ src/app/organization/[organizationId]/(internal)/home/page.tsx
@@ -23,6 +23,7 @@ function OrganizationHomePageContent() {
         if (!loading && !user) {
             router.push('/');
         } else if (!loading && user) {
+            console.log('User object for routing:', user);
             if (user.team === 'New User') {
                 router.replace(`/organization/${user.organizationId}/client/new/${user.userId}/home`);
             } else if (user.roleType === 'client') {
