98d25238f0fa74eca6b91c332e532ce6029729f8
Balaji Naik <balaji@habi.one>
2025-09-21 08:50:56 +0000

    <iframe width="560" height="315" src="https://www.youtube.com/embed/m52y

--- src/app/organization/[organizationId]/client/[clientId]/live/page.tsx
+++ src/app/organization/[organizationId]/client/[clientId]/live/page.tsx
@@ -13,11 +13,16 @@ export default function LivePage() {
       </div>
       <Card className="rounded-[50px] overflow-hidden">
         <CardContent className="p-0">
-          <div className="aspect-video bg-black flex items-center justify-center">
-            <div className="text-center text-white">
-                <Video className="w-16 h-16 mx-auto mb-4" />
-                <p>Live video feed placeholder</p>
-            </div>
+          <div className="aspect-video bg-black">
+            <iframe
+              className="w-full h-full"
+              src="https://www.youtube.com/embed/m52ynxtmOo?si=KlBbrybxtREhLrr_"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
           </div>
         </CardContent>
       </Card>
