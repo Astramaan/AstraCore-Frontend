e74eb73b39d8c1351f47ca029dab97ebf1d10a74
Balaji Naik <balaji@habi.one>
2025-09-21 10:39:07 +0000

    Give the Link strcuture to access the Organization, Client new user and

--- src/app/organization/[organizationId]/client/new/[newuserId]/layout.tsx
+++ src/app/organization/[organizationId]/client/new/[newuserId]/layout.tsx
@@ -1,16 +1,16 @@
-
 'use client';
 
 import React from 'react';
+import { ClientBottomNav } from '@/components/client-bottom-nav';
+import { ClientHeader } from '@/components/client-header';
 import { NewUserBottomNav } from '@/components/new-user-bottom-nav';
-import { NewUserHeader } from '@/components/new-user-header';
 
 export default function NewUserLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-background">
         <header className="sticky top-0 z-20 bg-background/80 backdrop-blur-sm">
            <div className="max-w-[1440px] mx-auto p-4">
-             <NewUserHeader />
+             <ClientHeader />
            </div>
         </header>
         <main className="w-full flex-1 overflow-y-auto bg-background pb-32">
