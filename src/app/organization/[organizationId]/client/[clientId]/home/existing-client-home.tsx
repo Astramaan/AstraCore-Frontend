b438cf0fe79405f5b8fd3ee66bbc5b5e228e8fa9
Balaji Naik <balaji@habi.one>
2025-09-22 11:21:41 +0000

    make the 54x54px (_for element src/app/organization/[organizationId]/cli

--- src/app/organization/[organizationId]/client/[clientId]/home/existing-client-home.tsx
+++ src/app/organization/[organizationId]/client/[clientId]/home/existing-client-home.tsx
@@ -184,7 +184,7 @@ const ChatCard = ({ pmPhoneNumber }: { pmPhoneNumber: string }) => (
         <CardContent className="p-0">
             <a href={`https://wa.me/91${pmPhoneNumber}`} target="_blank" rel="noopener noreferrer" className="flex flex-col items-center gap-2 text-center md:p-4 p-0">
                 <div className="w-14 h-14 rounded-full bg-green-light flex items-center justify-center">
-                    <div className="grid place-items-center h-10 w-10 rounded-full bg-white">
+                    <div className="grid place-items-center h-[54px] w-[54px] rounded-full bg-white">
                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="w-6 h-6 text-green"><rect width="256" height="256" fill="none"/><path d="M45.4,177A95.9,95.9,0,1,1,79,210.6h0L45.8,220a7.9,7.9,0,0,1-9.8-9.8L45.4,177Z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/><path d="M152.1,184A79.9,79.9,0,0,1,72,103.9,28,28,0,0,1,100,76h0a6.8,6.8,0,0,1,6,3.5l11.7,20.4a8.1,8.1,0,0,1-.1,8.1l-9.4,15.7h0a48,48,0,0,0,24.1,24.1h0l15.7-9.4a8.1,8.1,0,0,1,8.1-.1L176.5,150a6.8,6.8,0,0,1,3.5,6h0A28.1,28.1,0,0,1,152.1,184Z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="12"/></svg>
                         </div>
                     </div>
