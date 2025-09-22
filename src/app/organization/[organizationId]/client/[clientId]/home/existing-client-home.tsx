1f858d848f99c91779d79ff11325b0d6f509b0e7
Balaji Naik <balaji@habi.one>
2025-09-22 10:44:59 +0000

    i selected this element you changed the Payment and char card. (_for ele

--- src/app/organization/[organizationId]/client/[clientId]/home/existing-client-home.tsx
+++ src/app/organization/[organizationId]/client/[clientId]/home/existing-client-home.tsx
@@ -390,7 +390,7 @@ export default function ExistingClientHomePage() {
         <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-6 gap-8 md:p-8">
             {/* Timeline */}
             <div className="md:col-span-3 lg:col-span-4 order-2 md:order-1">
-                 <div className="mb-6 flex flex-col md:flex-row gap-4 justify-between">
+                 <div className="mb-6 flex flex-row gap-4 justify-between">
                     <Button
                         variant="outline"
                         onClick={() => setIsCompletedTasksSheetOpen(true)}
@@ -427,7 +427,7 @@ export default function ExistingClientHomePage() {
             </div>
 
             <aside className="md:col-span-2 lg:col-span-2 flex flex-col gap-4 order-1 md:order-2">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="grid grid-cols-2 gap-4">
                     <PaymentsDialog>
                         <PaymentCard />
                     </PaymentsDialog>
