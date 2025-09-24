daabd26dcba99b0b3114ad711f68dfd0ca57d3f0
Balaji Naik <balaji@habi.one>
2025-09-24 05:56:38 +0000

    I will adjust the layout to create a 16px space on the right side of the

--- src/app/otp-verification/page.tsx
+++ src/app/otp-verification/page.tsx
@@ -11,7 +11,7 @@ export default function OtpVerificationPage({ searchParams }: { searchParams: { 
 
           {/* Image Section */}
           <div className="relative w-full h-[250px] shrink-0 md:w-[396px] md:h-[507px] self-center">
-            <div className="h-full relative pl-4">
+            <div className="h-full relative pr-4">
               <Image 
                   src="/images/logoimage.png"
                   alt="Astramaan construction site"
@@ -24,7 +24,7 @@ export default function OtpVerificationPage({ searchParams }: { searchParams: { 
           </div>
           
           {/* Form Section */}
-          <div className="bg-card rounded-t-[50px] px-4 py-8 -mt-[69px] md:mt-0 relative flex flex-col md:py-6 md:pl-10 md:pr-6 md:w-[448px]">
+          <div className="bg-card rounded-t-[50px] px-4 py-8 -mt-[69px] md:mt-0 relative flex flex-col md:py-6 md:pl-6 md:pr-10 md:w-[448px]">
             <div className="mx-auto w-full max-w-md flex flex-col h-full">
               <HabiLogo className="md:mx-auto"/>
               <div className="mt-8 flex-grow flex flex-col">
