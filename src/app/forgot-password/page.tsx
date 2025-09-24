3e9488fb0adf03a4d4b057b734a679c84800f6b1
Balaji Naik <balaji@habi.one>
2025-09-24 05:31:47 +0000

    you didn't change anything

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -5,14 +5,12 @@ import Image from "next/image";
 
 export default function ForgotPasswordPage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
   return (
-    <div className="min-h-screen bg-background">
-      {/* Combined Layout for mobile and desktop */}
-      <div className="md:flex md:items-center md:justify-center md:h-screen md:p-4">
-        <div className="flex flex-col md:flex-row overflow-hidden md:rounded-[50px] bg-card text-card-foreground md:w-auto">
-
+    <div className="min-h-screen bg-background md:flex md:items-center md:justify-center md:p-4">
+      <div className="flex flex-col md:flex-row overflow-hidden md:rounded-[50px] bg-card text-card-foreground md:max-w-4xl w-full">
+          
           {/* Image Section */}
-          <div className="relative w-full h-[250px] shrink-0 md:w-[396px]">
-            <div className="md:p-4 h-full relative">
+          <div className="relative w-full h-[250px] shrink-0 md:w-[396px] md:h-auto">
+            <div className="h-full relative md:p-4">
               <Image 
                   src="/images/logoimage.png"
                   alt="Astramaan construction site"
@@ -23,19 +21,18 @@ export default function ForgotPasswordPage({ searchParams }: { searchParams: { [
               />
             </div>
           </div>
-          
+
           {/* Form Section */}
           <div className="bg-card rounded-t-[50px] px-4 py-8 -mt-[69px] md:mt-0 relative flex flex-col md:py-6 md:px-10 md:w-[448px]">
             <div className="mx-auto w-full max-w-md flex flex-col h-full">
-              <HabiLogo className="md:mx-auto"/>
-              <div className="mt-8 flex flex-col flex-grow">
+              <HabiLogo className="md:mx-auto" />
+              <div className="mt-8 flex-grow flex flex-col">
                 <SetPasswordForm flow="forgot-password" />
               </div>
             </div>
           </div>
 
         </div>
-      </div>
     </div>
   );
 }
