5a68c1855b49a23ca8349e08cffcee765592dd03
Balaji Naik <balaji@habi.one>
2025-09-25 10:58:28 +0000

    Give the another button of the Branding for the super admin. (_for eleme

--- src/app/organization/[organizationId]/(internal)/profile/super-admin-profile.tsx
+++ src/app/organization/[organizationId]/(internal)/profile/super-admin-profile.tsx
@@ -27,8 +27,7 @@ export default function SuperAdminProfile() {
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
                 <div className="lg:col-span-2 flex flex-col gap-6">
-                    <FeatureAccessCard />
-                    <BrandingWorkflowCard />
+                   {/* FeatureAccessCard and BrandingWorkflowCard are rendered via PersonalDetails now */}
                 </div>
                 <div className="flex flex-col gap-6">
                     <ActiveSessionsCard />
