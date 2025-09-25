948a492a550d7441347fb3921af7f3694f524ddc
Balaji Naik <balaji@habi.one>
2025-09-25 10:59:10 +0000

    Where is the Button?

--- src/app/organization/[organizationId]/(internal)/profile/super-admin-profile.tsx
+++ src/app/organization/[organizationId]/(internal)/profile/super-admin-profile.tsx
@@ -27,7 +27,8 @@ export default function SuperAdminProfile() {
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
                 <div className="lg:col-span-2 flex flex-col gap-6">
-                   {/* FeatureAccessCard and BrandingWorkflowCard are rendered via PersonalDetails now */}
+                   <FeatureAccessCard />
+                   <BrandingWorkflowCard />
                 </div>
                 <div className="flex flex-col gap-6">
                     <ActiveSessionsCard />
