4cea2683f170b0a019cd51d77acda44e5c40ab16
Balaji Naik <balaji@habi.one>
2025-09-25 12:25:27 +0000

    The ui doest fit in screen make it one below one in mobile screen (_for

--- src/app/organization/[organizationId]/(internal)/home/project-manager-home.tsx
+++ src/app/organization/[organizationId]/(internal)/home/project-manager-home.tsx
@@ -108,7 +108,7 @@ const ProjectSection = ({ project, onStageClick, onOpenCompletedTasks, onOpenUpc
               </div>
             </div>
         </div>
-        <div className="mt-6 flex gap-4 justify-between">
+        <div className="mt-6 flex flex-col md:flex-row gap-4 justify-between">
             <Button
                 variant="outline"
                 onClick={onOpenCompletedTasks}
