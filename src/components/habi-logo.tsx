4fd4f91a11556bf1c1e3f0cebbffe773a80e219c
Balaji Naik <balaji@habi.one>
2025-09-25 06:14:20 +0000

    use the primary color (_for element h2_)

--- src/components/habi-logo.tsx
+++ src/components/habi-logo.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export const HabiLogo = ({ className }: { className?: string }) => (
     <div className={cn("flex items-center gap-3", className)}>
         <div>
-            <h2 className="text-3xl font-bold tracking-tighter text-black">habi</h2>
+            <h2 className="text-3xl font-bold tracking-tighter text-primary">habi</h2>
             <p className="text-[0.5rem] tracking-[0.16em] text-center text-black">THE WAY OF LIVING</p>
         </div>
     </div>
