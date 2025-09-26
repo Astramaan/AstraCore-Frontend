ec96fe876563943353c6e4625cebdd3461e63885
Balaji Naik <balaji@habi.one>
2025-09-26 10:44:42 +0000

    reduce the width right side paymentstatus, Chatcard and recent site phot

--- src/app/organization/[organizationId]/client/[clientId]/home/existing-client-home.tsx
+++ src/app/organization/[organizationId]/client/[clientId]/home/existing-client-home.tsx
@@ -373,7 +373,7 @@ export default function ExistingClientHomePage() {
             </ProjectInfoHeader>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-7 2xl:grid-cols-5 gap-8 px-4 md:px-8">
+        <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-7 2xl:grid-cols-4 gap-8 px-4 md:px-8">
             {/* Timeline */}
             <div className="md:col-span-3 lg:col-span-5 2xl:col-span-3 order-2 md:order-1">
                  <div className="mb-6 flex flex-row gap-4 justify-between">
@@ -412,7 +412,7 @@ export default function ExistingClientHomePage() {
                 </div>
             </div>
 
-            <aside className="md:col-span-2 lg:col-span-2 2xl:col-span-2 flex flex-col gap-4 order-1 md:order-2">
+            <aside className="md:col-span-2 lg:col-span-2 2xl:col-span-1 flex flex-col gap-4 order-1 md:order-2">
                  <div className="flex flex-col gap-4">
                     <PaymentsDialog>
                        <PaymentCard />
@@ -435,7 +435,7 @@ export default function ExistingClientHomePage() {
     />
      <ImagePreviewDialog 
         open={previewState.open}
-        onOpenChange={(open) => !open && closeImagePreview()}
+        onOpenChange={(open) => !open && closePreview()}
         images={project.siteImages}
         startIndex={previewState.startIndex}
         title="Site Photo"
