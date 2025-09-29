ea2f0832d5d278f65008da13ca97b7e3a7c1fbff
Balaji Naik <balaji@habi.one>
2025-09-29 12:05:05 +0000

    Fix

--- src/app/organization/[organizationId]/client/new/[newuserId]/profile/page.tsx
+++ src/app/organization/[organizationId]/client/new/[newuserId]/profile/page.tsx
@@ -1,9 +1,8 @@
-
 import React, { Suspense } from 'react';
 import ClientProfilePageContent from '@/app/organization/[organizationId]/client/[clientId]/profile/client-profile-page-content';
 import { Skeleton } from '@/components/ui/skeleton';
 
-export default function ClientProfilePage() {
+export default function NewClientProfilePage() {
     return (
         <Suspense fallback={
             <div className="space-y-6 p-4">
@@ -22,5 +21,6 @@ export function generateStaticParams() {
     { organizationId: 'ORG-f9705032-d42a-46df-b799-87bcda629142', newuserId: 'newUser1' },
     { organizationId: 'ORG-f9705032-d42a-46df-b799-87bcda629142', newuserId: 'newUser2' },
     { organizationId: 'ORG-f9705032-d42a-46df-b799-87bcda629142', newuserId: '1e17e76f2486e270' },
+    { organizationId: 'ORG-f9705032-d42a-46df-b799-87bcda629142', newuserId: 'client123' },
   ];
 }
