a205ad8b4be9538431179c2d8b2dbe30f2f56080
Balaji Naik <balaji@habi.one>
2025-10-01 08:10:11 +0000

    you idiot remove the side bard entirely add the bottom nav for platform

--- src/app/platform/layout.tsx
+++ src/app/platform/layout.tsx
@@ -2,15 +2,11 @@
 'use client';
 
 import React from 'react';
-import { PlatformSidebar } from '@/components/platform-sidebar';
 import { PlatformBottomNav } from '@/components/platform-bottom-nav';
 
 function PlatformLayoutContent({ children }: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen bg-background flex">
-            <div className="hidden md:block w-64 border-r border-stone-300">
-                <PlatformSidebar />
-            </div>
             <div className="flex-1 flex flex-col">
                 <main className="flex-1 overflow-y-auto pb-20 md:pb-0">
                     {children}
