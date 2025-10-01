3ada7118b490f9a641d089bd925338067dc20451
Balaji Naik <balaji@habi.one>
2025-10-01 07:03:11 +0000

    Can i build with capacitor?

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ["https://6000-firebase-studio-1755838622226.cluster-nle52mxuvfhlkrzyrq6g2cwb52.cloudworkstations.dev"],
-  },
   transpilePackages: ['@ionic/react', '@ionic/core', '@stencil/core', 'ionicons'],
   images: {
     unoptimized: true,
