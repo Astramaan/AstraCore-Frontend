592fba14d4a9acd978ba1a07680c129ad1bca732
Balaji Naik <balaji@habi.one>
2025-10-01 07:40:47 +0000

    why build is failing?

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 import { type NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
   transpilePackages: ['@ionic/react', '@ionic/core', '@stencil/core', 'ionicons'],
   images: {
     unoptimized: true,
