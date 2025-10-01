7111f25a5fefac7229eb96052b86ff396d44da98
Balaji Naik <balaji@habi.one>
2025-10-01 07:58:19 +0000

    remove this (_for element <PlatformSidebar>_)

--- src/app/platform/layout.tsx
+++ src/app/platform/layout.tsx
@@ -2,14 +2,12 @@
 'use client';
 
 import React from 'react';
-import { PlatformSidebar } from '@/components/platform-sidebar';
+import { usePathname, useRouter } from 'next/navigation';
+import { Skeleton } from '@/components/ui/skeleton';
 
 function PlatformLayoutContent({ children }: { children: React.ReactNode }) {
     return (
         <div className="min-h-screen bg-background flex">
-            <div className="hidden md:block w-64 border-r border-stone-300">
-                <PlatformSidebar />
-            </div>
             <div className="flex-1 flex flex-col">
                 <main className="flex-1 overflow-y-auto">
                 {children}
