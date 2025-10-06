85df826a8e6f25fbd4a06fd05202e89c11e97fdc
Balaji Naik <balaji@habi.one>
2025-10-06 10:44:48 +0000

    Make this white color only in light mode (_for element <TeamIcon>_)

--- src/components/organization-header.tsx
+++ src/components/organization-header.tsx
@@ -76,11 +76,16 @@ export const OrganizationHeader = () => {
                     <Link href={`/organization/${organizationId}/teams`}>
                         <div className="relative p-px rounded-full bg-gradient-to-br from-white/50 to-white/0 dark:from-white/20 dark:to-white/0">
                              <Button className={cn(
-                                "rounded-full h-[54px] px-4 lg:px-10 text-base lg:text-lg font-medium flex items-center bg-black/20 dark:bg-black/30 text-white dark:text-foreground backdrop-blur-sm hover:bg-primary/10 hover:text-primary",
-                                isTeamsActive ? "bg-primary text-white dark:text-white" : "text-black dark:text-white"
+                                "rounded-full h-[54px] px-4 lg:px-10 text-base lg:text-lg font-medium flex items-center bg-black/20 dark:bg-black/30 backdrop-blur-sm hover:bg-primary/10",
+                                isTeamsActive ? "bg-primary text-white dark:text-primary-foreground" : "text-black dark:text-white"
                             )}>
-                                <TeamIcon className="mr-2 h-6 w-6"/>
-                                <span>{teamsButtonText}</span>
+                                <TeamIcon className={cn(
+                                    "mr-2 h-6 w-6",
+                                    isTeamsActive ? "text-white dark:text-black" : "text-white dark:text-white"
+                                )}/>
+                                <span className={cn(
+                                    isTeamsActive ? "text-white dark:text-black" : "text-white dark:text-white"
+                                )}>{teamsButtonText}</span>
                             </Button>
                         </div>
                     </Link>
