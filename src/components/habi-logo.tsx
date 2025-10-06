c47a6175a6051db09d6ee799324bf1474bfc4c6c
Balaji Naik <balaji@habi.one>
2025-10-06 08:45:23 +0000

    Astramaan_Logo.png use this for the logo it is in public folder

--- src/components/habi-logo.tsx
+++ src/components/habi-logo.tsx
@@ -1,12 +1,10 @@
 
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 import React from "react";
 
 export const HabiLogo = ({ className }: { className?: string }) => (
-    <div className={cn("flex items-center gap-3", className)}>
-        <div>
-            <h2 className="text-3xl font-bold tracking-tighter text-primary">habi</h2>
-            <p className="text-[0.5rem] tracking-[0.16em] text-center text-black">THE WAY OF LIVING</p>
-        </div>
+    <div className={cn("flex items-center", className)}>
+        <Image src="/Astramaan_Logo.png" alt="Astramaan Logo" width={120} height={40} />
     </div>
 );
