e1fa773bda15b8147a74135d9a396faed54d3b1a
Balaji Naik <balaji@habi.one>
2025-10-06 10:19:29 +0000

    Swap with Personal details (_for element src/app/organization/[organizat

--- src/app/organization/[organizationId]/(internal)/profile/super-admin-profile.tsx
+++ src/app/organization/[organizationId]/(internal)/profile/super-admin-profile.tsx
@@ -24,11 +24,11 @@ export default function SuperAdminProfilePage() {
     return (
         <div className="space-y-6">
             <div className="flex justify-between items-center">
-                <h2 className="text-2xl font-medium text-zinc-900 dark:text-white">My Profile</h2>
-                <Button variant="outline" onClick={() => router.back()} className="rounded-full h-[54px] px-6 text-lg bg-card hover:bg-primary/10 hover:text-primary hidden md:flex">
+                 <Button variant="outline" onClick={() => router.back()} className="rounded-full h-[54px] px-6 text-lg bg-card hover:bg-primary/10 hover:text-primary hidden md:flex">
                     <ChevronLeft className="mr-2 h-4 w-4" />
                     Back
                 </Button>
+                <h2 className="text-2xl font-medium text-zinc-900 dark:text-white">My Profile</h2>
             </div>
             <PersonalDetails memberId={user.userId} />
 
