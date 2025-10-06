1be111dbf58993d0c2982bdde21d385e43c1817a
Balaji Naik <balaji@habi.one>
2025-10-06 07:14:10 +0000

    Give gradient border like nav buttons (_for element <Button>_)

--- src/components/notification-popover.tsx
+++ src/components/notification-popover.tsx
@@ -48,10 +48,12 @@ export function NotificationPopover() {
     return (
         <Popover>
             <PopoverTrigger asChild>
-                 <Button variant="ghost" size="icon" className="bg-black/5 border border-white/20 backdrop-blur-sm rounded-full h-12 w-12 md:h-14 md:w-14 hover:bg-primary/10 hover:text-primary relative text-white">
-                    <NotificationBellIcon className="h-6 w-6" />
-                    <span className="absolute top-3 right-3 w-2.5 h-2.5 bg-red-500 rounded-full border-2 border-white" />
-                </Button>
+                <div className="relative p-px rounded-full bg-gradient-to-br from-white/50 to-white/0">
+                    <Button variant="ghost" size="icon" className="bg-black/5 backdrop-blur-sm rounded-full h-12 w-12 md:h-14 md:w-14 hover:bg-primary/10 hover:text-primary relative text-white">
+                        <NotificationBellIcon className="h-6 w-6" />
+                        <span className="absolute top-3 right-3 w-2.5 h-2.5 bg-red-500 rounded-full border-2 border-white" />
+                    </Button>
+                </div>
             </PopoverTrigger>
             <PopoverContent className="w-80 md:w-96 p-2 rounded-[50px] bg-white border-0" align="center">
                 <div className="p-4 flex items-center gap-2">
