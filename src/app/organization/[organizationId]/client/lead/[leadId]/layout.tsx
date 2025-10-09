4e888965da1c632c083b1defe58245bbe1d769da
Balaji Naik <balaji@habi.one>
2025-10-09 06:43:45 +0000

    Make it like client header (_for element header_)

--- src/app/organization/[organizationId]/client/lead/[leadId]/layout.tsx
+++ src/app/organization/[organizationId]/client/lead/[leadId]/layout.tsx
@@ -9,8 +9,14 @@ export default function LeadLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <div className="min-h-screen bg-background">
-        <header className="sticky top-0 z-20 bg-background/80 backdrop-blur-sm p-4 2xl:px-10">
-            <ClientHeader />
+        <header className="sticky top-2 z-20 px-2">
+          <div className="relative p-px rounded-full bg-gradient-to-br from-white/50 to-white/0 dark:from-white/20 dark:to-white/0">
+            <div className="relative w-full bg-black/20 rounded-full backdrop-blur-[5px] px-4 py-2">
+              <div className="max-w-[1440px] 2xl:max-w-none mx-auto">
+                <ClientHeader />
+              </div>
+            </div>
+          </div>
         </header>
         <main className="w-full flex-1 overflow-y-auto bg-background pb-32">
             {children}
