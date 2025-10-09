54e46d34ac60e82957269b099cea168a15dda391
Balaji Naik <balaji@habi.one>
2025-10-09 09:01:25 +0000

    Check the build issue and fix

--- src/app/organization/[organizationId]/client/[clientId]/layout.tsx
+++ src/app/organization/[organizationId]/client/[clientId]/layout.tsx
@@ -32,7 +32,7 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
                     <div className="relative p-px rounded-full bg-gradient-to-br from-white/50 to-white/0 dark:from-white/20 dark:to-white/0">
                       <div className="relative w-full bg-black/20 rounded-full backdrop-blur-[5px] px-4 py-2">
                         <div className="max-w-[1440px] 2xl:max-w-none mx-auto">
-                          
+                          <ClientHeader />
                         </div>
                       </div>
                     </div>
