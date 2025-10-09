06d8a7a743b5aa5b32f46ee93d3325e08e540211
Balaji Naik <balaji@habi.one>
2025-10-09 06:57:18 +0000

    make the blur rectangle like the client version lead header blur rectang

--- src/app/organization/[organizationId]/(internal)/layout.tsx
+++ src/app/organization/[organizationId]/(internal)/layout.tsx
@@ -9,8 +9,8 @@ export default function OrganizationInternalLayout({ children }: { children: Rea
   return (
     <div className="min-h-screen bg-background">
         <header className="sticky top-2 z-20 px-2">
-          <div className="relative p-px rounded-full bg-gradient-to-b from-white/50 to-white/0 dark:from-white/20 dark:to-white/0">
-            <div className="relative w-full bg-black/20 rounded-full backdrop-blur-[5px] p-4">
+          <div className="relative p-px rounded-full bg-gradient-to-br from-white/50 to-white/0 dark:from-white/20 dark:to-white/0">
+            <div className="relative w-full bg-black/20 dark:bg-black/30 rounded-full backdrop-blur-[5px] px-4 py-2">
               <div className="max-w-[1440px] 2xl:max-w-none mx-auto px-4 2xl:px-10">
                 <OrganizationHeader />
               </div>
