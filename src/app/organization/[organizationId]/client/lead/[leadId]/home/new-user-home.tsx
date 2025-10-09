c0962a33f9d5059e061093abe5f10e35151e5358
Balaji Naik <balaji@habi.one>
2025-10-09 09:57:53 +0000

    Check entire code base and fix the issue.

--- src/app/organization/[organizationId]/client/lead/[leadId]/home/new-user-home.tsx
+++ src/app/organization/[organizationId]/client/lead/[leadId]/home/new-user-home.tsx
@@ -159,7 +159,7 @@ export default function NewUserHomePage({ params }: { params: { organizationId: 
 
     return (
         <div className="relative">
-            <header className="sticky top-2 z-20 px-2">
+             <header className="sticky top-2 z-20 px-2">
                 <div className="relative p-px rounded-full bg-gradient-to-br from-white/50 to-white/0 dark:from-white/20 dark:to-white/0">
                     <div className="relative w-full bg-black/20 rounded-full backdrop-blur-[5px] px-4 py-2">
                         <div className="max-w-[1440px] 2xl:max-w-none mx-auto">
