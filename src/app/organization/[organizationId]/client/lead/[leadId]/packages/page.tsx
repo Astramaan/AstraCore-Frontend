3aca5f80a0221e64a271c11385367c5c020d57e7
Balaji Naik <balaji@habi.one>
2025-10-10 05:55:02 +0000

    look the contact team button shadow. make it to all side so user can see

--- src/app/organization/[organizationId]/client/lead/[leadId]/packages/page.tsx
+++ src/app/organization/[organizationId]/client/lead/[leadId]/packages/page.tsx
@@ -103,7 +103,7 @@ export default function PackagesPage() {
                 </div>
 
                 <div className="fixed bottom-28 left-0 right-0 py-4 bg-transparent z-10 text-center md:static md:p-0 md:pt-8">
-                    <Button asChild className="rounded-full h-[54px] px-8 text-lg w-full max-w-xs md:w-auto shadow-2xl">
+                    <Button asChild className="rounded-full h-[54px] px-8 text-lg w-full max-w-xs md:w-auto drop-shadow-2xl">
                         <Link href={`/organization/${organizationId}/client/lead/${leadId}/home#book-consultation-section`}>
                             Contact Team
                         </Link>
