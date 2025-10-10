cc24382b777eaa85caeeaea11ef8e920cd1941c2
Balaji Naik <balaji@habi.one>
2025-10-10 07:19:55 +0000

    Remove the padding from bottom. (_for element <ClientProfilePageContent>

--- src/app/organization/[organizationId]/client/lead/[leadId]/profile/page.tsx
+++ src/app/organization/[organizationId]/client/lead/[leadId]/profile/page.tsx
@@ -5,15 +5,13 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function LeadProfilePage() {
     return (
-        <div className="pb-32 md:pb-8">
-            <Suspense fallback={
-                <div className="space-y-6 p-4">
-                    <Skeleton className="h-48 w-full rounded-[50px]" />
-                    <Skeleton className="h-14 w-full rounded-full" />
-                </div>
-            }>
-                <ClientProfilePageContent />
-            </Suspense>
-        </div>
+        <Suspense fallback={
+            <div className="space-y-6 p-4">
+                <Skeleton className="h-48 w-full rounded-[50px]" />
+                <Skeleton className="h-14 w-full rounded-full" />
+            </div>
+        }>
+            <ClientProfilePageContent />
+        </Suspense>
     );
 }
