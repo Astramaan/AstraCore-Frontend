c837e966dfed0481a34cf983c391abb752c5a93b
Balaji Naik <balaji@habi.one>
2025-10-12 09:35:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8630621436-9ddb5",
+  "appId": "1:689439930769:web:d771d8d25b818d19bfc230",
+  "apiKey": "AIzaSyD2_M_b0dLzsAE-YCeiIBkRKQ8Z7sigp6A",
+  "authDomain": "studio-8630621436-9ddb5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "689439930769"
+};
