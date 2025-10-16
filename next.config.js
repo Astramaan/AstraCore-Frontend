20253f991d0af21414db5a823952a48d5836438b
Balaji Naik <balaji@astramaan.com>
2025-10-16 04:42:56 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // This allows the Next.js dev server to accept requests from the
-    // Firebase Studio environment.
-    allowedDevOrigins: [
-      "https://9000-firebase-studio-1755838622226.cluster-nle52mxuvfhlkrzyrq6g2cwb52.cloudworkstations.dev",
-    ],
-  },
   images: {
     remotePatterns: [
       {
