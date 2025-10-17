8034a837f738002af592779aea171a790e2b760c
Balaji Naik <balaji@astramaan.com>
2025-10-17 06:50:39 +0000

    Remove the sidebar in platform

--- src/app/platform/layout.tsx
+++ src/app/platform/layout.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 import React from "react";
-import { PlatformSidebar } from "@/components/platform-sidebar";
 import { UserProvider } from "@/context/user-context";
 import { PlatformBottomNav } from "@/components/platform-bottom-nav";
 
 function PlatformLayoutContent({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-background flex">
-      <div className="hidden md:block w-64 border-r border-stone-300">
-        <PlatformSidebar />
-      </div>
       <div className="flex-1 flex flex-col">
         <main className="flex-1 overflow-y-auto">{children}</main>
       </div>
