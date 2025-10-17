6cb05d4a5ba375c5cc8b1f12efc7cfdc1bd50ec0
Balaji Naik <balaji@astramaan.com>
2025-10-17 05:15:39 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8630621436-9ddb5",
-  "appId": "1:689439930769:web:d771d8d25b818d19bfc230",
-  "apiKey": "AIzaSyD2_M_b0dLzsAE-YCeiIBkRKQ8Z7sigp6A",
-  "authDomain": "studio-8630621436-9ddb5.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "689439930769"
+  "projectId": "ardent-spot-474911-r5",
+  "appId": "1:326639574895:web:ac27b689a742c2378a54c6",
+  "apiKey": "AIzaSyAB42K9WfA8R-X-baxqJk38n2A82W-1X-Y",
+  "authDomain": "ardent-spot-474911-r5.firebaseapp.com",
+  "measurementId": "G-L9CPJ5B6QB",
+  "messagingSenderId": "326639574895"
 };
