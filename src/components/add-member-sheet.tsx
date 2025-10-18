458538c856665c707eaf82d90f1bcc5223c89af5
Balaji Naik <balaji@habi.one>
2025-10-18 05:22:21 +0000

    if click on this the dropdown is not working. (_for element <SelectTrigg

--- src/components/add-member-sheet.tsx
+++ src/components/add-member-sheet.tsx
@@ -307,7 +307,7 @@ export function AddMemberSheet({
         <SheetContent
           side="bottom"
           className="p-0 m-0 flex flex-col bg-card text-card-foreground transition-all h-full md:h-[90vh] md:max-w-md md:mx-auto rounded-t-[50px] border-none"
-           onInteractOutside={(e) => {
+          onInteractOutside={(e) => {
             if (
               (e.target as HTMLElement).closest(
                 "[data-radix-popper-content-wrapper]",
