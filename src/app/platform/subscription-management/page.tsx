d9b1b9df03ac56f43c13dfa9abcb04a4f9c3efab
Balaji Naik <balaji@astramaan.com>
2025-10-20 06:13:04 +0000

    Make this card content vertically align (_for element <CardContent>_)

--- src/app/platform/subscription-management/page.tsx
+++ src/app/platform/subscription-management/page.tsx
@@ -282,7 +282,7 @@ export default function SubscriptionManagementPage() {
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           <Card className="rounded-[50px] squircle lg:col-span-2">
-            <CardContent className="p-6">
+            <CardContent className="p-6 flex items-center justify-center">
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full">
                 <QuickLink
                   icon={<UserCheck className="h-6 w-6 text-green-700" />}
